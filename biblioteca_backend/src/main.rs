20ca01864392817050a6acee2e427aca2b088917
jumpcutfindo <danielhoe97@gmail.com>
2023-07-13 15:33:07 +0800

    Add simple GET endpoint

--- biblioteca_backend/src/main.rs
+++ biblioteca_backend/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{
+    routing::{get},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // Initialize tracing
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    // Create router
+    let app = axum::Router::new()
+        .route("/", get(root));
+
+    // Run app using hyper, listens on port 3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("la-biblioteca server listening on {}", addr);
+    
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// Sample handler that responds with static string
+async fn root() -> &'static str {
+    "Hello World!"
 }
