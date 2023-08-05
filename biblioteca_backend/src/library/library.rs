fbacee660533938f197cf5ed47fa056a79bafd74
jumpcutfindo <danielhoe97@gmail.com>
2023-08-05 23:25:29 +0800

    Add router and linkage to module

--- biblioteca_backend/src/library/library.rs
+++ biblioteca_backend/src/library/library.rs
@@ -1,3 +1,13 @@
+use axum::{Router, routing::{delete, post}};
+
+use crate::AppState;
+
+pub fn library_router() -> Router<AppState> {
+    Router::new()
+        .route("/books/:id/borrow", post(borrow_book))
+        .route("/books/:id/return", delete(return_book))
+}
+
 pub async fn borrow_book() {
 
 }
