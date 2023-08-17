1a6ca905d907b970f41bf43a3beafef079befd4f
jumpcutfindo <danielhoe97@gmail.com>
2023-08-17 21:28:41 +0800

    Implement checking of borrow status

--- biblioteca_backend/src/library/db.rs
+++ biblioteca_backend/src/library/db.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use axum::extract::State;
 use rusqlite::Result;
 use uuid::Uuid;
@@ -5,17 +7,38 @@ use chrono::prelude::*;
 
 use crate::AppState;
 
-use super::model::BookState;
+use super::{model::BookBorrowState, error::LibraryError};
 
 pub async fn add_borrow_entry_to_db(
     State(state): State<AppState>,
     user_id: Uuid,
     book_id: Uuid,
-) -> Result<()> {
-    state.db_pool.get().unwrap().execute(
+) -> Result<(), LibraryError> {
+    let conn = state.db_pool.get().unwrap();
+
+    // Check if the book is currently borrowed
+    match conn.query_row(
+        "SELECT * FROM map_users_to_borrowed_books WHERE book_id = ?1 ORDER BY timestamp",
+        [book_id], 
+        |row| {
+            row.get(3)
+        }) {
+            Ok::<BookBorrowState, _>(latest_entry) => {
+                // If entry exists, check if it's "Borrowed"
+                match latest_entry {
+                    BookBorrowState::Borrowed => return Err(LibraryError::BookBorrowed),
+                    BookBorrowState::Returned => {},
+                }
+            },
+            Err(_) => {
+                // If entry doesn't exist, we continue
+            }
+        }
+
+    conn.execute(
         "INSERT INTO map_users_to_borrowed_books (user_id, book_id, timestamp, action) VALUES (?1, ?2, ?3, ?4)",
-        (user_id, book_id, Utc::now(), BookState::Borrowed),
-    )?;
+        (user_id, book_id, Utc::now(), BookBorrowState::Borrowed),
+    ).unwrap();
 
     Ok(())
 }
