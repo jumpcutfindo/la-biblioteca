bf23abebd6e5ba131c5c6cf010392c8876da21b9
jumpcutfindo <danielhoe97@gmail.com>
2023-08-17 19:53:29 +0800

    Move user table creation to own func

--- biblioteca_backend/src/database.rs
+++ biblioteca_backend/src/database.rs
@@ -12,55 +12,7 @@ pub fn setup_db() -> Result<Pool<SqliteConnectionManager>> {
     let pool = r2d2::Pool::new(manager).unwrap();
 
     setup_catalog_tables(&pool);
-
-    tracing::debug!("Creating table 'user_roles'...");
-    pool.get()
-        .unwrap()
-        .execute(
-            "CREATE TABLE IF NOT EXISTS user_roles (
-                id                      BLOB PRIMARY KEY,
-                role_name               TEXT NOT NULL,
-                num_borrowable_books    INT NOT NULL
-            )",
-            ()
-        )?;
-
-    tracing::debug!("Inserting some default roles into 'user_roles'...");
-    let binding = pool.get().unwrap();
-    let mut user_role_stmt = binding.prepare(
-        "INSERT OR IGNORE INTO user_roles (id, role_name, num_borrowable_books) VALUES (?1, ?2, ?3)"
-    )?;
-
-    user_role_stmt.execute((Uuid::parse_str("f4658962-1237-4518-b55c-1f44986a4604").unwrap(), String::from("admin"), 0))?;
-    user_role_stmt.execute((Uuid::parse_str("ded1bba9-84aa-4138-8f71-b27cfe6a51a0").unwrap(), String::from("adult_user"), 8))?;
-    user_role_stmt.execute((Uuid::parse_str("27b122ab-b9e7-4f9b-ad7e-368340cfec76").unwrap(), String::from("child_user"), 4))?;
-
-    tracing::debug!("Creating table 'users'...");
-    pool.get()
-        .unwrap()
-        .execute(
-            "CREATE TABLE IF NOT EXISTS users (
-                id              BLOB PRIMARY KEY,
-                username        TEXT UNIQUE NOT NULL
-            )", 
-            ()
-        )?;
-
-    tracing::debug!("Creating table 'map_users_to_user_roles'...");
-    pool.get()
-        .unwrap()
-        .execute(
-            "CREATE TABLE IF NOT EXISTS map_users_to_user_roles (
-                user_id             BLOB NOT NULL,
-                user_role_id        BLOB NOT NULL,
-                CONSTRAINT fk_users
-                    FOREIGN KEY(user_id) REFERENCES users(id)
-                    ON DELETE CASCADE,
-                CONSTRAINT fk_user_roles
-                    FOREIGN KEY(user_role_id) REFERENCES user_roles(id)
-            )",
-        ()
-        )?;
+    setup_user_tables(&pool);
 
     tracing::debug!("Creating table 'map_users_to_borrowed_books'...");
     pool.get()
@@ -128,6 +80,61 @@ pub fn setup_catalog_tables(pool: &Pool<SqliteConnectionManager>) {
                     ON DELETE CASCADE
             )", 
             ()
+        )
+        .unwrap();
+}
+
+pub fn setup_user_tables(pool: &Pool<SqliteConnectionManager>) {
+    tracing::debug!("Creating table 'user_roles'...");
+    pool.get()
+        .unwrap()
+        .execute(
+            "CREATE TABLE IF NOT EXISTS user_roles (
+                id                      BLOB PRIMARY KEY,
+                role_name               TEXT NOT NULL,
+                num_borrowable_books    INT NOT NULL
+            )",
+            ()
+        )
+        .unwrap();
+
+    tracing::debug!("Inserting some default roles into 'user_roles'...");
+    let binding = pool.get().unwrap();
+    let mut user_role_stmt = binding.prepare(
+        "INSERT OR IGNORE INTO user_roles (id, role_name, num_borrowable_books) VALUES (?1, ?2, ?3)"
+    )
+    .unwrap();
+
+    user_role_stmt.execute((Uuid::parse_str("f4658962-1237-4518-b55c-1f44986a4604").unwrap(), String::from("admin"), 0)).unwrap();
+    user_role_stmt.execute((Uuid::parse_str("ded1bba9-84aa-4138-8f71-b27cfe6a51a0").unwrap(), String::from("adult_user"), 8)).unwrap();
+    user_role_stmt.execute((Uuid::parse_str("27b122ab-b9e7-4f9b-ad7e-368340cfec76").unwrap(), String::from("child_user"), 4)).unwrap();
+
+    tracing::debug!("Creating table 'users'...");
+    pool.get()
+        .unwrap()
+        .execute(
+            "CREATE TABLE IF NOT EXISTS users (
+                id              BLOB PRIMARY KEY,
+                username        TEXT UNIQUE NOT NULL
+            )", 
+            ()
+        )
+        .unwrap();
+
+    tracing::debug!("Creating table 'map_users_to_user_roles'...");
+    pool.get()
+        .unwrap()
+        .execute(
+            "CREATE TABLE IF NOT EXISTS map_users_to_user_roles (
+                user_id             BLOB NOT NULL,
+                user_role_id        BLOB NOT NULL,
+                CONSTRAINT fk_users
+                    FOREIGN KEY(user_id) REFERENCES users(id)
+                    ON DELETE CASCADE,
+                CONSTRAINT fk_user_roles
+                    FOREIGN KEY(user_role_id) REFERENCES user_roles(id)
+            )",
+        ()
         )
         .unwrap();
 }
