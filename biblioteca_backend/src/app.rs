f1f2812d97d56a509bceb894d7a1adb5d6fe5048
jumpcutfindo <danielhoe97@gmail.com>
2023-08-18 14:44:20 +0800

    Modularise router creation

--- biblioteca_backend/src/app.rs
+++ biblioteca_backend/src/app.rs
@@ -1,5 +1,25 @@
+use axum::Router;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
+
+use crate::{catalog::{books::books_router, authors::authors_router}, users::users::users_router, library::library::library_router, database::setup_db};
+
+pub fn app() -> Router {
+    // Create an in-memory db
+    let pool = setup_db().unwrap();
+
+    let state = AppState {
+        db_pool: pool,
+    };
+
+    // Create router
+    Router::new()
+        .merge(books_router())
+        .merge(authors_router())
+        .merge(users_router())
+        .merge(library_router())
+        .with_state(state)
+}
 
 #[derive(Clone)]
 pub struct AppState {
--- biblioteca_backend/src/main.rs
+++ biblioteca_backend/src/main.rs
@@ -7,12 +7,6 @@ mod users;
 mod library;
 mod error;
 
-use axum::Router;
-
-use database::setup_db;
-
-use crate::{app::AppState, catalog::{authors::authors_router, books::books_router}, users::users::users_router, library::library::library_router};
-
 #[tokio::main]
 async fn main() {
     // Initialize tracing
@@ -20,20 +14,7 @@ async fn main() {
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
-    // Create an in-memory db
-    let pool = setup_db().unwrap();
-
-    let state = AppState {
-        db_pool: pool,
-    };
-
-    // Create router
-    let app = Router::new()
-        .merge(books_router())
-        .merge(authors_router())
-        .merge(users_router())
-        .merge(library_router())
-        .with_state(state);
+    let app = app::app();
 
     // Run app using hyper, listens on port 3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
