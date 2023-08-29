de1e6ebb7c0d4953fe23aab326c2e09ff3a8ba9a
jumpcutfindo <danielhoe97@gmail.com>
2023-08-29 21:33:52 +0800

    Add querier for mock db

--- biblioteca_backend/tests/mocker/db.rs
+++ biblioteca_backend/tests/mocker/db.rs
@@ -1,7 +1,7 @@
 use std::fs::remove_file;
 
 use biblioteca_backend::{catalog::model::{Author, Book}, database::setup_db};
-use r2d2::Pool;
+use r2d2::{Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
 use uuid::Uuid;
 
@@ -45,5 +45,51 @@ impl MockDatabaseBuilder {
 
     pub fn build(self) -> Pool<SqliteConnectionManager> {
         return self.connection;
+    }
+}
+
+pub struct MockDatabaseQuerier {
+    pub connection: PooledConnection<SqliteConnectionManager>,
+}
+
+impl MockDatabaseQuerier {
+    pub fn of(pool: &mut Pool<SqliteConnectionManager>) -> MockDatabaseQuerier {
+        return MockDatabaseQuerier {
+            connection: pool.get().unwrap(),
+        }
+    }
+
+    pub fn contains_book(&self, book: &Book) -> bool {
+        match self.connection.query_row::<i32,_,_>(
+            "SELECT COUNT(*) FROM books WHERE id = ?1 AND name = ?2 AND description = ?3 AND language = ?4", 
+            (&book.id, &book.name, &book.description, &book.language),
+            |row| Ok(row.get(0)?)
+        ) {
+            Ok(count) => return count == 1,
+            Err(_) => return false,
+        }
+    }
+
+    pub fn contains_author(&self, author: &Author) -> bool {
+        match self.connection.query_row::<i32,_,_>(
+            "SELECT COUNT(*) FROM authors WHERE id = ?1 AND name = ?2 AND description = ?3 AND country = ?4", 
+            (&author.id, &author.name, &author.description, &author.country),
+            |row| Ok(row.get(0)?)
+        ) {
+            Ok(count) => return count == 1,
+            Err(_) => return false,
+        }
+    }
+
+    pub fn contains_book_author_mapping(&self, book_id: &Uuid, author_id: &Uuid) -> bool {
+        match self.connection.query_row::<i32,_,_>(
+            "SELECT COUNT(*) FROM map_books_to_authors WHERE book_id = ?1 AND author_id = ?2", 
+            (book_id, author_id),
+            |row| Ok(row.get(0)?)
+        ) {
+            Ok(count) => return count == 1,
+            Err(_) => return false,
+        }
+
     }
 }
