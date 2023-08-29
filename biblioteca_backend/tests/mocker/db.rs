a24b1ba49c81fbbcc07845763df85395992577da
jumpcutfindo <danielhoe97@gmail.com>
2023-08-29 10:32:17 +0800

    Add mock db creation to mocker

--- biblioteca_backend/tests/mocker/db.rs
+++ biblioteca_backend/tests/mocker/db.rs
@@ -1,15 +1,21 @@
-use biblioteca_backend::catalog::model::{Author, Book};
-use r2d2::PooledConnection;
+use biblioteca_backend::{catalog::model::{Author, Book}, database::setup_db};
+use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use uuid::Uuid;
 
 pub struct MockDatabaseBuilder {
-    connection: PooledConnection<SqliteConnectionManager>,
+    connection: Pool<SqliteConnectionManager>,
 }
 
 impl MockDatabaseBuilder {
+    pub fn create(database_path: String) -> MockDatabaseBuilder {
+        return MockDatabaseBuilder { 
+            connection: setup_db(database_path).unwrap(),
+        };
+    }
+
     pub fn with_author(&self, author: Author) -> &MockDatabaseBuilder {
-        self.connection.execute(
+        self.connection.get().unwrap().execute(
             "INSERT INTO authors (id, name, description, country) VALUES (?1, ?2, ?3, ?4)",
             (&author.id, &author.name, &author.description, &author.country),
         ).unwrap();
@@ -18,12 +24,12 @@ impl MockDatabaseBuilder {
     }
 
     pub fn with_book(&self, book: Book, author_id: Uuid) -> &MockDatabaseBuilder {
-        self.connection.execute(
+        self.connection.get().unwrap().execute(
             "INSERT INTO books (id, name, description, language) VALUES (?1, ?2, ?3, ?4)",
             (&book.id, &book.name, &book.description, &book.language),
         ).unwrap();
 
-        self.connection.execute(
+        self.connection.get().unwrap().execute(
             "INSERT INTO map_books_to_authors (book_id, author_id) VALUES (?1, ?2)",
             (&book.id, author_id),
         ).unwrap();
