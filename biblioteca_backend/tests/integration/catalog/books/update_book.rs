7e7daa304b8f22221417332bfbf0a1198209657b
jumpcutfindo <danielhoe97@gmail.com>
2023-08-31 18:31:54 +0800

    Add test skeletons for update_book

---
+++ biblioteca_backend/tests/integration/catalog/books/update_book.rs
@@ -0,0 +1,19 @@
+#[tokio::test]
+fn update_book_correct_parameters_successful() {
+
+}
+
+#[tokio::test]
+fn update_book_incorrect_parameters_successful() {
+
+}
+
+#[tokio::test]
+fn update_book_existing_author_successful() {
+
+}
+
+#[tokio::test]
+fn update_book_nonexistent_author_unsuccessful() {
+    
+}
