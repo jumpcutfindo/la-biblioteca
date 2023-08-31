0f885fcaa0d4dc48c44f2494a301d4560cb29f04
jumpcutfindo <danielhoe97@gmail.com>
2023-08-31 15:18:42 +0800

    Add implementation of list books tests

--- biblioteca_backend/tests/books_tests.rs
+++ biblioteca_backend/tests/books_tests.rs
@@ -252,7 +252,109 @@ async fn list_books_successful() {
 }
 
 #[tokio::test]
-async fn list_books_with_search_successful() {
+async fn list_books_with_name_search_successful() {
+    let database_path = "list_books_with_name_search_successful.sqlite";
+
+    let author = MockCatalog::new_author().build();
+
+    let book_a = MockCatalog::new_book()
+        .name("Alice in Wonderland".to_string())
+        .build();
+
+    let book_b = MockCatalog::new_book()
+        .name("Alice in Wonderland 2".to_string())
+        .build();
+
+    let book_c = MockCatalog::new_book()
+        .name("Harry Potter and the Philosopher's Stone".to_string())
+        .build();
+
+    let db = MockDatabaseBuilder::create(database_path.to_string())
+        .with_author(&author)
+        .with_book(&book_a, &author.id)
+        .with_book(&book_b, &author.id)
+        .with_book(&book_c, &author.id)
+        .build();
+
+    let app = create_mock_app(db);
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method("GET")
+                .uri("/books?name=Alice")
+                .body(Body::empty())
+                .unwrap()
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK, "checking if response is OK");
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    let returned_books: Vec<Book> = serde_json::from_slice(&body).unwrap();
+
+    {
+        assert_eq!(returned_books.len() == 2, true);
+        for book in returned_books.iter() {
+            assert_eq!(book.name.contains("Alice"), true);
+        }
+    }
+    
+    MockDatabaseBuilder::teardown(database_path.to_string());
+}
+
+#[tokio::test]
+async fn list_books_with_language_search_successful() {
+    let database_path = "list_books_with_language_search_successful.sqlite";
+
+    let author = MockCatalog::new_author().build();
+
+    let book_a = MockCatalog::new_book()
+        .language("English".to_string())
+        .build();
+
+    let book_b = MockCatalog::new_book()
+        .language("English".to_string())
+        .build();
+
+    let book_c = MockCatalog::new_book()
+        .language("Chinese".to_string())
+        .build();
+
+    let db = MockDatabaseBuilder::create(database_path.to_string())
+        .with_author(&author)
+        .with_book(&book_a, &author.id)
+        .with_book(&book_b, &author.id)
+        .with_book(&book_c, &author.id)
+        .build();
+
+    let app = create_mock_app(db);
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method("GET")
+                .uri("/books?language=English")
+                .body(Body::empty())
+                .unwrap()
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK, "checking if response is OK");
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    let returned_books: Vec<Book> = serde_json::from_slice(&body).unwrap();
+
+    {
+        assert_eq!(returned_books.len() == 2, true);
+        for book in returned_books.iter() {
+            assert_eq!(book.language.contains("English"), true);
+        }
+    }
+    
+    MockDatabaseBuilder::teardown(database_path.to_string());
 
 }
 
