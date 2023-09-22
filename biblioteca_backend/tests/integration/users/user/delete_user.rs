a24edee40bca035b150d45fd5476c999c37168a7
jumpcutfindo <danielhoe97@gmail.com>
2023-09-22 22:30:14 +0800

    Add delete_user tests

---
+++ biblioteca_backend/tests/integration/users/user/delete_user.rs
@@ -0,0 +1,101 @@
+use hyper::{Request, Body, StatusCode};
+use tower::ServiceExt;
+use uuid::Uuid;
+
+use crate::mocker::{db::{MockDatabaseBuilder, MockDatabaseQuerier}, app::create_mock_app, users::MockUserBase};
+
+#[tokio::test]
+async fn delete_user_existing_user_successful() {
+    let database_path = "delete_user_existing_user_successful.sqlite";
+
+    let user_a = MockUserBase::new_user().build();
+    let user_b = MockUserBase::new_user().build();
+    let user_role = MockUserBase::new_user_role().build();
+
+    let db = MockDatabaseBuilder::create(database_path.to_string())
+        .with_user_role(&user_role)
+        .with_user(&user_a, &user_role)
+        .with_user(&user_b, &user_role)
+        .build();
+
+    let app = create_mock_app(db);
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method("DELETE")
+                .uri(format!("/users/{}", user_a.id))
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.status(),
+        StatusCode::NO_CONTENT,
+        "checking if response is OK"
+    );
+
+    {
+        let querier = MockDatabaseQuerier::create(database_path.to_string());
+        assert_eq!(
+            querier.contains_user(&user_a),
+            false,
+            "checking if user was removed properly"
+        );
+
+        assert_eq!(
+            querier.contains_user_user_role_mapping(&user_a.id, &user_role.id),
+            false,
+            "checking if user_a to user_role mapping was removed properly"
+        );
+    }
+
+    MockDatabaseBuilder::teardown(database_path.to_string());
+}
+
+#[tokio::test]
+async fn delete_user_non_existent_user_successful() {
+    let database_path = "delete_user_non_existent_user_successful.sqlite";
+
+    let user_role = MockUserBase::new_user_role().build();
+    let user = MockUserBase::new_user().build();
+
+    let incorrect_uuid = Uuid::new_v4();
+
+    let db = MockDatabaseBuilder::create(database_path.to_string())
+        .with_user_role(&user_role)
+        .with_user(&user, &user_role)
+        .build();
+
+    let app = create_mock_app(db);
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method("DELETE")
+                .uri(format!("/users/{}", incorrect_uuid))
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.status(),
+        StatusCode::NO_CONTENT,
+        "checking if response is OK"
+    );
+
+    {
+        let querier = MockDatabaseQuerier::create(database_path.to_string());
+        assert_eq!(
+            querier.contains_user(&user),
+            true,
+            "checking if no users were removed"
+        );
+    }
+
+    MockDatabaseBuilder::teardown(database_path.to_string());
+}
